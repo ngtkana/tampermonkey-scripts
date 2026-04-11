e6b661f6411f06c9e9fbeae1c7c95187f8b4e639
Kana Nagata <ngtkana@gmail.com>
2026-04-11 23:38:17 +0900

    learn placeholder

--- nico-commons-ngram/src/main.rs
+++ nico-commons-ngram/src/main.rs
@@ -1,3 +1,23 @@
+mod learn;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Learn
+    Learn,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Learn => learn::learn(),
+    }
 }
